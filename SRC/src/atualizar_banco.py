77cb0cce1b5147b3c75d15be6d789018fdc35183
Geandro Ribeiro Costa <geandro.sara@gmail.com>
2025-07-02 21:28:52 -0300

    alteração do banco de dados para incluir o campo "cliente" na tabela

--- SRC/src/atualizar_banco.py
+++ SRC/src/atualizar_banco.py
@@ -1,22 +1,26 @@
-# ACRESCENTAR A COLUNA CLIENTE NA TABELA DE PRODUÇÃO
+# #ACRESCENTAR A COLUNA CLIENTE NA TABELA DE PRODUÇÃO
 
-import sqlite3
+# import sqlite3
 
-def atualizar_tabela_banco():
-    conn = sqlite3.connect('sistema_plotter.db')
-    cursor = conn.cursor()
+# def atualizar_tabela_producao():
+#     conn = sqlite3.connect('sistema_plotter.db')
+#     cursor = conn.cursor()
 
-    try:
-        cursor.execute('ALTER TABLE producao ADD COLUMN cliente TEXT')
-        conn.commit()
-        print("Coluna 'cliente' adicionada à tabela 'producao' com sucesso.")
-    except sqlite3.OperationalError as e:
-        if "duplicate column name: cliente" in str(e):
-            print("A coluna 'cliente' já existe na tabela 'producao'.")
-        else:
-            print(f"Erro ao adicionar coluna 'cliente': {e}")
+#     try:
+#         cursor.execute('ALTER TABLE producao ADD COLUMN cliente TEXT')
+#         conn.commit()
+#         print("Coluna 'cliente' adicionada com sucesso à tabela 'producao'.")
+#     except sqlite3.OperationalError as e:
+#         if "duplicate column name" in str(e):
+#             print("A coluna 'cliente' já existe.")
+#         else:
+#             print(f"Erro ao adicionar coluna: {e}")
 
-    conn.close()
-    if __name__ == "__main__":
-        atualizar_tabela_banco()
-        print("Banco de dados atualizado com sucesso.")
+#     conn.close()
+
+# if __name__ == "__main__":
+#     atualizar_tabela_producao()
+
+        
+
+   
